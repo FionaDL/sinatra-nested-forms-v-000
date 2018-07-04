8004d963ade8aa03170be099640aa3069ba13cb8
Fiona Lapham <fionadlapham@gmail.com>
2018-07-04 10:15:15 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,24 +2,24 @@ require './environment'
 
 module FormsLab
   class App < Sinatra::Base
-
-    get "/" do
+    get '/' do
       erb :root
     end
 
-    get "/new" do
-      erb :pirates/new
+    get '/new' do
+      erb :'pirates/new'
     end
 
-    post "/pirates" do
-      @pirates = Pirate.new(params[:pirate])
+    post '/pirates' do
+      @pirate = Pirate.new(params[:pirate])
 
-     params[:pirate][:ships].each do |info|
-       Ship.new(info)
-     end
-      erb :pirates/show
+      params[:pirate][:ships].each do |details|
+        Ship.new(details)
+      end
+      @ships = Ship.all
+
+      erb :'pirates/show'
     end
-    # code other routes/actions here
-
+    
   end
 end
