bc70275de376d85860bbb79d82192a7315306818
Fiona Lapham <fionadlapham@gmail.com>
2018-07-04 09:21:31 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,7 +2,15 @@ require './environment'
 
 module FormsLab
   class App < Sinatra::Base
+    get "/new" do
+      erb :new
+    end
 
+    post "/pirates" do
+      @pirates = Pirate.new(params[:pirate])
+      binding.pry
+      erb :show
+    end
     # code other routes/actions here
 
   end
